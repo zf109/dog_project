35166733154f8990b3954728ac5423563d810468
zf109 <zhe.feng0018@gmail.com>
2017-07-02 18:10:13 +0100

    ver0.0.1

--- dog_app/views.py
+++ dog_app/views.py
@@ -34,43 +34,16 @@ def upload_file():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'image.jpg'))
-            result = dog_breed_guesser_from_path('tmp/image.jpg')
+            file_location = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_location)
+            result = dog_breed_guesser_from_path(file_location)
 
-            # return redirect(url_for('uploaded_file',
-            #                         filename=filename, test_result=result))
-            return render_template('image_upload.html', test_result=result)
+            return render_template('image_upload.html',
+                                    filename=filename, test_result=result)
+            # return render_template('image_upload.html', test_result=result)
     return render_template('image_upload.html', test_result=None)
-    # return '''
-    # <!doctype html>
-    # <title>Upload new File</title>
-    # <h1>Upload new File</h1>
-    # <form method=post enctype=multipart/form-data>
-    #   <p><input type=file name=file>
-    #      <input type=submit value=Upload>
-    # </form>
-    # '''
-# @app.route('/', methods=['GET', 'POST'])
-# @app.route('/upload', methods=['GET', 'POST'])
-# def upload_file():
-#     result=None
-#     if request.method == 'POST':
-#         file_b64 = request.form['b64file']
-#         print("requested")
-#         if file_b64:
-#             # Decode the image
-#             file = base64.b64decode(file_b64)
-#             img = Image.open(io.BytesIO(file))
-#             img.save('tmp/imge.jpg')
-#             print("image saved")
-#             result = dog_breed_guesser_from_path('tmp/imge.jpg')
-#     return render_template('image_upload.html', test_result=result)
 
-@app.route('/show/<test_result>/<filename>')
-def uploaded_file(filename):
-    filename = 'http://127.0.0.1:5000/uploads/' + filename
-    return render_template('show_image.html', filename=filename, test_result=test_result)
 
 @app.route('/uploads/<filename>')
-def send_file(filename):
+def serve_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
